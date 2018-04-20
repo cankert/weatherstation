79d9c053e5ea01e6c92efbe9a3e2944357cd54e6
Christian Ankert <christian.ankert@diva-e.com>
2018-04-20 10:41:17 +0200

    Fixed API

--- routes/weather.js
+++ routes/weather.js
@@ -4,11 +4,18 @@ var router = express.Router();
 /* GET users listing. */
 router.post('/', function(req, res, next) {
     var weather = req.body;
+    var data = {
+        "date":weather.date,
+        "temperature": weather.temperature,
+        "pressure": weather.pressure
+    }
+    console.log('Received Data from API: ');
+    console.log(weather);
     var db = req.db;
     var collection = db.get('weatherdata');
-    collection.insert(weather, function(err, result){
+    collection.insert(data, function(err, result){
         res.send(
-            (err === null) ? {msg: '' } : {msg: err }
+            (err === null) ? {msg: 'Data received' } : {msg: err }
 
         );
         console.log('Posted Data to API');
